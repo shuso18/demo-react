21c74e2379b51a962230645ec9fdca63228af286
shuso18 <78402372+shuso18@users.noreply.github.com>
2022-12-24 23:53:30 +0530

    Update index.js
    naya

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+var greet = "Good Afternoon";
+
+const dabba = {
+  width:"800px",
+  height:"80px",
+  margin:"auto",
+  backgroundColor:"aliceblue",
+  textAlign:"center",
+  marginTop:"340px",
+  borderRadius:"20px"
+}
+
+const rabba = {
+  fontWeight:"Bold",
+  color:"red",
+  paddingTop:"13.5px",
+  fontSize:"45px"
+}
+
+const dhabba = {
+  
+}
+
+const dt = new Date().getHours();
+
+
+if(dt >= 12 && dt < 19){
+  greet = "Good Afternoon";
+  dhabba.color = "orange";
+}
+else if( dt >=19){
+  greet = "Good Night";
+  dhabba.color = "black";
+  
+}
+else{
+  greet = "Good Morning";
+  dhabba.color = "green"
+}
+
+ReactDOM.render(
+  <>
+    <div style= {dabba}>
+        <h1 style={rabba}>Hello Shubham, <span style={dhabba}>{greet}</span> </h1>
+    </div>
+  </>,
+
+
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
