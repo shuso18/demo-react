1ec7ccd1fc13dae70345502a01eefe14793a6bc5
shuso18 <78402372+shuso18@users.noreply.github.com>
2022-12-27 00:41:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,44 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Cards from './Cards';
+import Sdata from './Sdata';
 
+function ncards(val){
+  return(
+    <Cards 
+    imgsrc={val.imgsrc}
+    namesr={val.namesr}
+    descrsr={val.descrsr}
+    linksr={val.linksr}
+  />
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <div className='hedd'><h1>Top 5 on Hotstar</h1></div>
+  <div className='hedd'><h1>Top 5 on Disney+Hotstar</h1></div>
   <div className='bigcont'>
-  <Cards 
-    imgsrc='https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/4295/754295-v'
-    namesr='Avatar'
-    descrsr='A Disney-Hotstar Original Series'
-    linksr='https://www.hotstar.com/in/movies/avatar/1770000948'
-  />
-  <Cards 
-    imgsrc='https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/old_images/vertical/MOVIE/4189/1000074189/1000074189-v'
-    namesr='Drishyam'
-    descrsr='A Disney-Hotstar Original Series'
-    linksr='https://www.hotstar.com/in/movies/drishyam/1000074189'
-  />
-  <Cards 
-    imgsrc='https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/4954/894954-v'
-    namesr='The Good Wife'
-    descrsr='A Disney-Hotstar Original Series'
-    linksr='https://www.hotstar.com/in/movies/the-good-wife/1835045497'
-  />
-  <Cards 
-    imgsrc='https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/5541/675541-v'
-    namesr='The Jungle Book'
-    descrsr='A Disney-Hotstar Original Series'
-    linksr='https://www.hotstar.com/in/movies/the-jungle-book/1260018310'
-  />
-  <Cards 
-    imgsrc='https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/old_images/vertical/MOVIE/7441/1000087441/1000087441-v'
-    namesr='Masaan'
-    descrsr='A Disney-Hotstar Original Series'
-    linksr='https://www.hotstar.com/in/movies/masaan/1000087441'
-  />
+    {Sdata.map(ncards)}
   </div>
   </>
 );
-
