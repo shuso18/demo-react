1dea3199bb3965998a7152e1e10d0fda09b04242
shuso18 <78402372+shuso18@users.noreply.github.com>
2022-12-27 00:38:01 +0530

    data file

---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+const Sdata = [
+    {
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/4295/754295-v',
+        namesr:'Avatar',
+        descrsr:'Disney+Hotstar Originals',
+        linksr:'https://www.hotstar.com/in/movies/avatar/1770000948'
+    },
+    {
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/old_images/vertical/MOVIE/4189/1000074189/1000074189-v',
+        namesr:'Drishyam',
+        descrsr:'Disney+Hotstar Originals',
+        linksr:'https://www.hotstar.com/in/movies/drishyam/1000074189'   
+    },
+    {
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/4954/894954-v',
+        namesr:'The Good Wife',
+        descrsr:'Disney+Hotstar Originals',
+        linksr:'https://www.hotstar.com/in/movies/the-good-wife/1835045497'  
+    },
+    {
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/5541/675541-v',
+        namesr:'The Jungle Book',
+        descrsr:'Disney+Hotstar Originals',
+        linksr:'https://www.hotstar.com/in/movies/the-jungle-book/1260018310' 
+    },
+    {
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/old_images/vertical/MOVIE/7441/1000087441/1000087441-v',
+        namesr:'Masaan',
+        descrsr:'Disney+Hotstar Originals',
+        linksr:'https://www.hotstar.com/in/movies/masaan/1000087441'   
+    }
+
+];
+
+export default Sdata;
